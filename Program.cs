9e47d624a0c52ae062bee530f84bede4d8175a87
davidgm05 <dapnestic05@gmail.com>
2023-04-14 09:24:23 +0200

    asasas

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, este es un programa en el que se pueden realizar operaciones de dos cifras");
+Console.WriteLine ("Introduzca un número");
+var primernumero=Console.WriteLine();
+int num1 = int.Parse(primernumero);
+Console.WriteLine("Introduce otro número");
+var segundonumero=Console.WriteLine();
+
